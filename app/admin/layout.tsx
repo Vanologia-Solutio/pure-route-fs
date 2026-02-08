fd9b13981b07168c2ca19d319dc7a0717f00b5ad
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2026-02-08 20:43:40 +0700

    Refactor layout structure by creating separate MainLayout and AdminLayout components; simplify RootLayout by removing unnecessary main wrapper, enhancing code organization and readability.

---
+++ app/admin/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <main className='max-w-6xl mx-auto min-h-[calc(100vh-120px)] px-4'>
+      <div className='py-8'>{children}</div>
+    </main>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -33,9 +33,7 @@ export default function RootLayout({
       >
         <ClientProvider>
           <NavigationBar />
-          <main className='max-w-6xl mx-auto min-h-[calc(100vh-120px)] px-4'>
-            <div className='py-8'>{children}</div>
-          </main>
+          {children}
           <Footer />
         </ClientProvider>
       </body>
