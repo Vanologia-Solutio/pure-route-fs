fc32e1f17a0c59cc88c59b73f9c7e8a14ba4bc60
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2026-02-11 20:42:55 +0700

    Add Loading component with spinner for improved user experience during data loading

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/general/loader-spinner";
+
+export default function Loading() {
+  return (
+    <div className='flex items-center justify-center min-h-[calc(100vh-120px)] p-4 lg:px-0 lg:py-4'>
+      <LoadingSpinner size="xl" message="Loading..." />
+    </div>
+  )
+}
