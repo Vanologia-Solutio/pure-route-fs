0d1c2b01797157ddef1b0dd7b779791fe7cbf56d
Ryu Alvano <117072949+BlvckM1nds@users.noreply.github.com>
2026-02-20 12:46:25 +0700

    Update email contact in SupportPage and payment template to use new email address pureroutepeps@proton.me for improved communication.

--- app/(main)/support/page.tsx
+++ app/(main)/support/page.tsx
@@ -8,10 +8,10 @@ export default function SupportPage() {
         If you have any questions or need assistance, please contact us at
       </p>
       <a
-        href='mailto:pureroutepeptides@proton.me'
+        href='mailto:pureroutepeps@proton.me'
         className='text-green-700 hover:text-green-800 hover:underline'
       >
-        pureroutepeptides@proton.me
+        pureroutepeps@proton.me
       </a>
     </Fragment>
   )
